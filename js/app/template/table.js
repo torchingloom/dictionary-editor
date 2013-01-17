8df66a8044e8416bb0a3890233cc01a6d24e416f
Ivan Hilkov <strange.mole@gmail.com>
2013-01-18 03:21:38 +0400

    style and output td according to fields_in_list

--- js/app/template/table.js
+++ js/app/template/table.js
@@ -2,10 +2,6 @@ window.JST = {};
 window.JST['dictionary'] = _.template(''+
   '<% var type = "content-widescreen"; if (settings.display_type === 1) { type = "content-horizotal" }; if (settings.display_type === 2) { type = "content-vertical" }; %>'+
 
-
-  '<div class="content <%= type %>">'+
-    '<div class="content-list">'+
-
   '<div class="cont_table_bar right clearfix">'+
     '<span title="кнопка">кнопка</span>'+
   '</div>'+
@@ -15,26 +11,34 @@ window.JST['dictionary'] = _.template(''+
     '<span title="кнопка">кнопка</span>'+
   '</div>'+
 
+  '<div class="content <%= type %>">'+
+    '<div class="content-list">'+
+
+
   '<div class="wrapper_head">'+
   '<div class="cont_table">'+
 
+    '<table id="tables"  class="table table-striped">'+
 
-    '<table class="table table-striped">'+
+
       '<thead>'+
         '<tr>'+
-    '<% _.each( settings.fields, function(item) {%>'+
-          '<th><%= item.title %></th>'+
+    '<% _.each( settings.fields_in_list, function(key) {%>'+
+      '<th><%= settings["fields"][key]["title"] %></th>'+
     '<% }); %>'+
         '</tr>'+
       '</thead>'+
+
+
       '<tbody>'+
     '<% _.each( filds, function(fild) {%>'+
         '<tr data-id="<%= fild.id %>">'+
-      '<% _.each( fild, function(value) {%>'+
-          '<td class="item"><%= value %></td>'+
-      '<% }); %>'+
+    '<% _.each( settings.fields_in_list, function(key) {%>'+
+          '<td class="item"><%= fild[key] %></td>'+
+    '<% }); %>'+
         '</tr>'+
     '<% }); %>'+
+
       '</tbody>'+
     '</table>'+
 
@@ -44,11 +48,14 @@ window.JST['dictionary'] = _.template(''+
   '</div>'+
 
   '<div class="content-item">'+
+    '<div class="card_item">'+
     'Выберете строку'+
+    '</div>'+
   '</div>'+
 
 '</div>'
 );
+
 window.JST['cards'] = _.template(''+
   '<div class="cards">'+
     '<div class="cont_item">'+
